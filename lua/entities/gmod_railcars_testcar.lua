3214385dd929fcc0d0ac3250108f5ad8357e2b99
Titus Studios Media Group <titusstudiosmediagroup@gmail.com>
2020-11-18 22:56:19 +1300

    Update gmod_railcars_testcar.lua

--- lua/entities/gmod_railcars_testcar.lua
+++ lua/entities/gmod_railcars_testcar.lua
@@ -8,16 +8,16 @@ ENT.RenderGroup	= RENDERGROUP_OPAQUE
 ENT.Editable = true
 ENT.Spawnable = true
 ENT.AdminOnly = false
---ENT.DisableDuplicator = true
---ENT.DoNotDuplicate = true
 
-local Model = "models/hanksabutt/rollingstock/bethgon_coalporter/bethgon_coalporter_irlskins.mdl"
-local BogieModel = "models/gsgtrainprops/parts/trucks/barber_s2hd_36in_phx.mdl"
-local Bogie1Pos = Vector(245.8,0,1.4)
-local Bogie2Pos = Vector(-245.8,0,1.4)
-local HandBrakePos = Vector(-300,16.55,50.9)
-local CouplerPos = Vector(-320,0,8)
-local CouplerPos2 = Vector(320,0,8)
+local Model = "models/lazpack/freightcars/trinity_3230_pd.mdl"
+local BogieModel = "models/magtrains/trucks/barber_s2_rsg.mdl"
+local Bogie1Pos = Vector(201,0,-0.5)
+local Bogie2Pos = Vector(-201,0,-0.5)
+local HandBrakePos = Vector(267,-19,65)
+local CouplerPos = Vector(278,0,8)
+local CouplerPos2 = Vector(-278,0,8)
+local CutbarPos = Vector(263,-43,1)
+local CutbarPos2 = Vector(-263,43,1)
 local Ambient = "titus's locomotive sound expansion pack/resources/railvehicle/freightstock/resources/wheels/defective/s_freightdefectivewheel02.wav"
 local BrakeAmbient = "titus's locomotive sound expansion pack/resources/railvehicle/freightstock/resources/brakes/s_freightbraking03.wav"
 local HandBrakeChain = {"titus's locomotive sound expansion pack/plugins/dlc/coalhopperbethogonii/content/view/audio/resources/handbrake/s_bethgonhandbrakechain01.wav",
@@ -26,7 +26,9 @@ local HandBrakeChain = {"titus's locomotive sound expansion pack/plugins/dlc/coa
                         "titus's locomotive sound expansion pack/plugins/dlc/coalhopperbethogonii/content/view/audio/resources/handbrake/s_bethgonhandbrakechain04.wav",
                         "titus's locomotive sound expansion pack/plugins/dlc/coalhopperbethogonii/content/view/audio/resources/handbrake/s_bethgonhandbrakechain05.wav"}
 local HandBrake = 0 --Don't Change
-local SpawnHeight = 35 --temp
+local CanCouple = 1 --Don't Change
+local CanCouple2 = 1 --Don't Change
+local CouplerRopePoint = 169
 
 
 local function ModelCreate(self,class,parent,model,position,angle,collisiongroup,rendermode)
@@ -40,6 +42,7 @@ local function ModelCreate(self,class,parent,model,position,angle,collisiongroup
     ent:SetCollisionGroup(collisiongroup)
     ent:GetPhysicsObject():SetMaterial("friction_00")
     ent:PhysWake()
+    ent:SetNWBool("LuaRailcars",true) 
 
     if parent == nil then
         ent:SetPos(position)
@@ -55,6 +58,15 @@ local function PlayerWithinBounds(ply,otherPly,dist)
 	return ply:EyePos():DistToSqr(otherPly) < (dist*dist)
 end
 
+local function EntityWithinBounds(ply,otherPly,dist)
+	return ply:GetPos():DistToSqr(otherPly:GetPos()) < (dist*dist)
+end
+
+local function EntityOutsideBounds(ply,otherPly,dist)
+	return ply:GetPos():DistToSqr(otherPly:GetPos()) > (dist*dist)
+end
+
+
 if SERVER then
     function ENT:Initialize()
         self:SetModel(Model)
@@ -64,15 +76,29 @@ if SERVER then
         self:SetUseType(SIMPLE_USE)
         self:SetPos(self:GetPos()+Vector(0,0,35))
         self:SetCollisionGroup(20)
+        self:SetNWBool("LuaRailcar", true) 
+        HandBrake = 0 --Don't Change
+        CanCouple = 1 --Don't Change
+        CanCouple2 = 1 --Don't Change
 
         if constraint.CanConstrain(self,0) then
-            Bogie1 = ModelCreate(self,"prop_physics",nil,BogieModel,self:LocalToWorld(Bogie1Pos),self:GetAngles(),0,0)
+            Bogie1 = ModelCreate(self,"prop_physics",nil,BogieModel,self:LocalToWorld(Bogie1Pos),self:GetAngles()+Angle(0,90,0),0,0)
+            Bogie1:SetBodygroup(1,2)
+            Bogie1:SetSubMaterial(0,"models/proppertextures/wheel")
+            Bogie1:SetSubMaterial(7,"models/proppertextures/wheel")
             constraint.Axis(Bogie1,self,0,0,Vector(0,0,0),Vector(0,0,0),0,0,0,1,Vector(0,0,1))
-            Bogie2 = ModelCreate(self,"prop_physics",nil,BogieModel,self:LocalToWorld(Bogie2Pos),self:GetAngles(),0,0)
+
+            Bogie2 = ModelCreate(self,"prop_physics",nil,BogieModel,self:LocalToWorld(Bogie2Pos),self:GetAngles()+Angle(0,90,0),0,0)
+            Bogie2:SetBodygroup(1,2)
+            Bogie2:SetSubMaterial(0,"models/proppertextures/wheel")
+            Bogie2:SetSubMaterial(7,"models/proppertextures/wheel")
             constraint.Axis(Bogie2,self,0,0,Vector(0,0,0),Vector(0,0,0),0,0,0,1,Vector(0,0,1))
+
             self:DeleteOnRemove(Bogie1)
             self:DeleteOnRemove(Bogie2)
+            self.Bogies = {Bogie1,Bogie2}
         end
+
 
         self.AmbientTrack = CreateSound(self,Ambient)
         self.AmbientTrack:PlayEx(1,0)
@@ -107,7 +133,7 @@ if SERVER then
             self.HandBrakeSound = CreateSound(self,HandBrakeChain[math.random(1,5)])
             self.HandBrakeSound:PlayEx(1,100)
 
-            if HandBrake then
+            if HandBrake == 1 then
                 if IsValid(Bogie1) then
                     Bogie1:GetPhysicsObject():SetMaterial("metal")
                 end
@@ -127,25 +153,93 @@ if SERVER then
                 WireLib.TriggerOutput(self,"Handbrake",HandBrake)
             end
         end
+
+        --[[
+        if PlayerWithinBounds(activator,self:LocalToWorld(CutbarPos),30) then
+            if constraint.FindConstraints(self.Bogies[1],"Rope") then
+                constraint.RemoveConstraints(self.Bogies[1],"Rope")
+            end
+        end
+
+        if PlayerWithinBounds(activator,self:LocalToWorld(CutbarPos2),30) then
+            if constraint.FindConstraints(self.Bogies[2],"Rope") then
+                constraint.RemoveConstraints(self.Bogies[2],"Rope")
+            end
+        end
+        ]]--
     end
 
     function ENT:Think()
-        local velocity = self:GetPhysicsObject():GetVelocity():Length()
-        local velocityclamped = math.Clamp(velocity/5,0,250)
-        self.AmbientTrack:ChangePitch(velocityclamped)
-        self.AmbientBrake:ChangePitch(velocityclamped*HandBrake)
+        local CouplerFind = ents.FindInSphere(self:LocalToWorld(CouplerPos),100)
+        local CouplerFind2 = ents.FindInSphere(self:LocalToWorld(CouplerPos2),100)
+        local Velocity = self:GetPhysicsObject():GetVelocity():Length()
+        local VelocityClamped = math.Clamp(Velocity/5,0,250)
+        self.AmbientTrack:ChangePitch(VelocityClamped)
+        self.AmbientBrake:ChangePitch(VelocityClamped*HandBrake)
+
+        for index,Entity in pairs(CouplerFind) do
+            if Entity:GetClass() == "prop_physics" then
+                if Entity:GetNWBool("LuaRailcars",false) ~= false then
+                    if constraint.Find(self,Entity,"Axis",0,0) == nil then
+                        if constraint.Find(self.Bogies[1],Entity,"Rope",0,0) then return end
+                        
+                        if CanCouple == 1 then
+                            if EntityWithinBounds(self.Bogies[1],Entity,CouplerRopePoint) then
+                                timer.Simple(0,function()
+                                    constraint.Rope(self.Bogies[1],Entity,0,0,Vector(0,0,0),Vector(0,0,0),self.Bogies[1]:GetPos():Distance(Entity:GetPos()),0,0,1.5,"cable/cable",true)
+                                    self.CoupleSound = CreateSound(self,"opencontrol/misc/couple1.wav")
+                                    self.CoupleSound:PlayEx(1,100)
+                                    CanCouple = 0
+                                end)
+                            end
+                        else
+                            if EntityOutsideBounds(self.Bogies[1],Entity,200) then
+                                CanCouple = 1
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        for index,Entity in pairs(CouplerFind2) do
+            if Entity:GetClass() == "prop_physics" then
+                if Entity:GetNWBool("LuaRailcars",false) ~= false then
+                    if constraint.Find(self,Entity,"Axis",0,0) == nil then
+                        if constraint.Find(self.Bogies[2],Entity,"Rope",0,0) then return end
+                        
+                        if CanCouple == 1 then
+                            if EntityWithinBounds(self.Bogies[2],Entity,CouplerRopePoint) then
+                                timer.Simple(0,function()
+                                    constraint.Rope(self.Bogies[2],Entity,0,0,Vector(0,0,0),Vector(0,0,0),self.Bogies[2]:GetPos():Distance(Entity:GetPos()),0,0,1.5,"cable/cable",true)
+                                    self.CoupleSound = CreateSound(self,"opencontrol/misc/couple1.wav")
+                                    self.CoupleSound:PlayEx(1,100)
+                                    CanCouple = 0
+                                end)
+                            end
+                        else
+                            if EntityOutsideBounds(self.Bogies[2],Entity,200) then
+                                CanCouple = 1
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
 else
     function ENT:Draw()
         self:DrawModel()
         render.DrawWireframeBox(self:GetPos(),self:GetAngles(),self:OBBMins(),self:OBBMaxs(),Color(255,0,0),false) --car bounding
-        --render.DrawWireframeBox(self:LocalToWorld(Bogie1Pos),Bogie1:GetAngles(),Bogie1:OBBMins(),Bogie1:OBBMaxs(),Color(255,0,0),false) --bogie bounding
-        --render.DrawWireframeBox(self:LocalToWorld(Bogie2Pos),Bogie2:GetAngles(),Bogie2:OBBMins(),Bogie2:OBBMaxs(),Color(255,0,0),false) --bogie bounding
         render.DrawWireframeBox(self:LocalToWorld(Bogie1Pos),self:GetAngles(),Vector(10,10,10),Vector(-10,-10,-10),Color(0,255,0),false) --bogie
         render.DrawWireframeBox(self:LocalToWorld(Bogie2Pos),self:GetAngles(),Vector(10,10,10),Vector(-10,-10,-10),Color(0,255,0),false) --bogie
         render.DrawWireframeBox(self:LocalToWorld(HandBrakePos),self:GetAngles(),Vector(4,12,12),Vector(-4,-12,-12),Color(0,255,0),false) --handbrake
         render.DrawWireframeBox(self:LocalToWorld(CouplerPos),self:GetAngles(),Vector(10,10,8),Vector(-10,-10,-8),Color(0,0,255),false) --coupler
         render.DrawWireframeBox(self:LocalToWorld(CouplerPos2),self:GetAngles(),Vector(10,10,8),Vector(-10,-10,-8),Color(0,0,255),false) --coupler
+        render.DrawWireframeBox(self:LocalToWorld(CutbarPos),self:GetAngles(),Vector(3,4,7),Vector(-3,-4,-7),Color(191,90,242),false) --cutbar
+        render.DrawWireframeBox(self:LocalToWorld(CutbarPos2),self:GetAngles(),Vector(3,4,7),Vector(-3,-4,-7),Color(191,90,242),false) --cutbar
+        render.DrawWireframeSphere(self:LocalToWorld(CouplerPos),100,10,10,Color(100,210,255)) --coupler finder
+        render.DrawWireframeSphere(self:LocalToWorld(CouplerPos2),100,10,10,Color(100,210,255)) --coupler finder
     end
     return
 end
